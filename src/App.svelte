56b5ebb7bc7dccd1acbc565f2341bc7ed00b5cba
SiMiTaKu <color.ui>
2022-09-04 11:37:01 +0900

    Edit Main Visual

--- src/App.svelte
+++ src/App.svelte
@@ -4,9 +4,13 @@
 </script>
 
 <main>
-	<img src={img_src} alt="prof-icon.jpg">
-	<h1>Hello {name}!</h1>
-	<div class="test-message">これはテストサイトだぜ</div>
+	<section id="main-visual">
+		<img src={img_src} alt="prof-icon.jpg" class="icon">
+		<h1 class="title">Hello {name}!</h1>
+	</section>
+	<div class="test-area">
+		<p class="test-area__message">これはテストサイトだぜ</p>
+	</div>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
@@ -14,20 +18,44 @@
 	main {
 		text-align: center;
 		padding:    0;
-		max-width:  240px;
+		width:      100%;
 		margin:     0 auto;
 	}
 
-	h1 {
-		color:          #ff3e00;
-		text-transform: uppercase;
-		font-size:      4em;
-		font-weight:    100;
+	#main-visual {
+		height:     200px;
+		padding:    60px;
+		text-align: left;
 	}
 
-	.test-message {
-		font-size:   large;
-		font-weight: bold;
+	#main-visual .icon{
+		display:       inline-block;
+		width:         200px;
+		margin-right:  50px;
+		border-radius: 100px;
+	}
+
+
+
+	#main-visual .title{
+		display:        inline-block;
+		color:          #ff3e00;
+		font-size:      4em;
+		font-weight:    100;
+		vertical-align: top;
+	}
+
+	.test-area {
+		width:            1024px;
+		margin:           0 auto;
+		background-color: gray;
+		border-radius:    8px;
+		height:           50px;
+	}
+	.test-area__message {
+	    font-size:   large;
+	    font-weight: bold;
+		color:       white;
 	}
 	@media (min-width: 640px) {
 		main {
